25f594b666eee61a2731cb5d135e96b4c76e36e7
Handole Kim <handolekim@Handoles-MBP.frontierlocal.net>
2018-07-23 15:28:51 -0400

    function and interface practice added

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "Han",
+    age: 29,
+    gender: "male"
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "Han",
+    age: 29,
+    gender: "male"
+};
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+}
+
+console.log(sayHi(person));
+
+export {};
